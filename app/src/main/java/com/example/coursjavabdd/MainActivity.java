0175d23a7bf31fc0e2ee9180c13ffb4fbaeb04ee
Wexop <mattheurtin@gmail.com>
2022-11-14 14:36:26 +0100

    create Two activity empty + button to navigate to them

--- app/src/main/java/com/example/coursjavabdd/MainActivity.java
+++ app/src/main/java/com/example/coursjavabdd/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.coursjavabdd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAddStudent;
+    private Button buttonSeeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAddStudent = (Button) findViewById(R.id.button_add_student);
+        buttonSeeResult = (Button) findViewById(R.id.button_see_result);
+
+        buttonAddStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addStudentActivity = new Intent(MainActivity.this, AddStudentActivity.class);
+                startActivity(addStudentActivity);
+            }
+        });
+
+        buttonSeeResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent seeResultActivity = new Intent(MainActivity.this, getResultActivity.class);
+                startActivity(seeResultActivity);
+            }
+        });
+
+
     }
 }
